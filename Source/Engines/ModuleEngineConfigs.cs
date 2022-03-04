6a8e56881f54b3167163b74f9d5ebc9df2d8ae4d
DRVeyl <DRVeyl@gmail.com>
2022-03-04 18:22:25 -0500

    Properly make Styles self-initialize
    Because GUIStyle *textures* seem to get cleaned up on scene changes, Styles needs to reinitialize on each scene change.
    Just make it into its own Monobehaviour and KSPAddon and let it take care of itself.  Remove need for other files to try to track its initialization altogether.

--- Source/Engines/ModuleEngineConfigs.cs
+++ Source/Engines/ModuleEngineConfigs.cs
@@ -1233,7 +1233,6 @@ namespace RealFuels
         private Rect guiWindowRect = new Rect(0, 0, 0, 0);
         private string myToolTip = string.Empty;
         private int counterTT;
-        private bool styleSetup = false;
         private bool editorLocked = false;
 
         private int toolTipWidth => EditorLogic.fetch.editorScreen == EditorScreen.Parts ? 220 : 300;
@@ -1253,12 +1252,6 @@ namespace RealFuels
 
             if (inPartsEditor && part.symmetryCounterparts.FirstOrDefault(p => p.persistentId < part.persistentId) is Part)
                 return;
-
-            if (!styleSetup)
-            {
-                styleSetup = true;
-                Styles.InitStyles();
-            }
 
             if (guiWindowRect.width == 0)
             {
